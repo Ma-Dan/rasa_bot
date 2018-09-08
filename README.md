32ccec2c2a46727092a65cca2da40a34fbe117b4
马丹 <dan@madandeMacBook-Air.local>
2018-09-08 10:56:06 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# 整理：基于RASA-NLU和RASA-CORE的任务型ChatBot
+
+## 组件版本
+1. Python 3
+2. TensorFlow 1.8.0
+3. Keras 2.2.2
+4. scikit-learn 0.19.1
+5. mitie 0.5.0 (安装方式：pip install git+https://github.com/mit-nlp/MITIE.git)
+6. rasa-nlu 0.13.1
+7. rasa-core 0.10.4
+
+## 资料来源
+1. MITIE预训练词向量 来自：[https://github.com/crownpku/Rasa_NLU_Chi](https://github.com/crownpku/Rasa_NLU_Chi)
+2. rasa-nlu和rasa-core示例配置和语料 来自：[https://github.com/zqhZY/_rasa_chatbot](https://github.com/zqhZY/_rasa_chatbot)
+这里仅整理为一个完整的包含NLU、Dialog训练和测试，ChatBot在线学习和使用的例子。
+
+## 准备工作
+1. git clone https://github.com/Ma-Dan/rasa_bot
+2. 下载MITIE预训练词向量total_word_feature_extractor_zh.dat，并放入rasa_bot/wordvector目录，下载链接：https://pan.baidu.com/s/1q9_v4o4riQKE9ZbsyvFWjQ 密码: 5j4h
+3. （可选）使用自己训练的MITIE词向量，方法：[http://blog.rubenxiao.com/posts/nlp-mitie.html](http://blog.rubenxiao.com/posts/nlp-mitie.html)
+
+## 训练和测试
+### RASA-NLU训练
+```shell
+python bot.py train-nlu
+> INFO:rasa_nlu.training_data.loading:Training data format of data/nlu.json is rasa_nlu
+> INFO:rasa_nlu.training_data.training_data:Training data stats: 
+> 	- intent examples: 169 (13 distinct intents)
+> 	- Found intents: 'inform_other_phone', 'inform_current_phone', 'unknown_intent', 'goodbye', 'thanks', 'inform_time', 'inform_item', 'greet', 'request_management', 'confirm', 'inform_package', 'deny', 'request_search'
+> 	- entity examples: 102 (4 distinct entities)
+> ...
+> Part I: train segmenter
+> ...
+> Part II: train segment classifier
+```
+### RASA-NLU测试(API调用测试，可用于前端程序调用RASA-NLU服务)
+```shell
+python -m rasa_nlu.server -c data/nlu_model_config.json --path models
+curl -XPOST localhost:5000/parse -d '{"q":"你好", "project":"ivr", "model":"demo"}'
+```
+### RASA-CORE训练
+```shell
+python bot.py train-dialogue
+```
+### RASA-CORE测试(API调用测试，可用于前端程序调用ChatBot服务)
+```shell
+python -m rasa_core.server -p 5005 -d models/dialogue -u models/ivr/demo -o out.log
+curl -XPOST localhost:5005/conversations/default/parse -d '{"query":"帮我查话费"}'
+curl -XPOST localhost:5005/conversations/default/continue -d '{"executed_action": "utter_greet", "events": []}'
+```
+### ChatBot测试(命令行直接跟ChatBot对话)
+```shell
+python bot.py run
+```
+![ChatBot测试](./run.png)
+### ChatBot在线学习(命令行交互训练ChatBot对话)
+```shell
+python bot.py run online-train
+```
+![ChatBot在线学习](./online_train.png)
---
+++ bot.py
@@ -0,0 +1,174 @@
+# -*- coding: UTF-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import argparse
+import logging
+import warnings
+
+from rasa_core.actions import Action
+from rasa_core.agent import Agent
+from rasa_core.channels.console import ConsoleInputChannel
+from rasa_core.events import SlotSet
+from rasa_core.interpreter import RasaNLUInterpreter
+from rasa_core.policies.keras_policy import KerasPolicy
+from rasa_core.policies.memoization import MemoizationPolicy
+
+logger = logging.getLogger(__name__)
+
+support_search = ["消费", "流量"]
+
+
+def extract_item(item):
+    """
+    check if item supported, this func just for lack of train data.
+    :param item: item in track, eg: "流量"、"查流量"
+    :return:
+    """
+    if item is None:
+        return None
+    for name in support_search:
+        if name in item:
+            return name
+    return None
+
+
+class ActionSearchConsume(Action):
+    def name(self):
+        return 'action_search_consume'
+
+    def run(self, dispatcher, tracker, domain):
+        item = tracker.get_slot("item")
+        item = extract_item(item)
+        if item is None:
+            dispatcher.utter_message("您好，我现在只会查话费和流量")
+            dispatcher.utter_message("你可以这样问我：“帮我查话费”")
+            return []
+
+        time = tracker.get_slot("time")
+        if time is None:
+            dispatcher.utter_message("您想查询哪个月的消费？")
+            return []
+        # query database here using item and time as key. but you may normalize time format first.
+        dispatcher.utter_message("好，请稍等")
+        if item == "流量":
+            dispatcher.utter_message("您好，您{}共使用{}二百八十兆，剩余三十兆。".format(time, item))
+        else:
+            dispatcher.utter_message("您好，您{}共消费二十八元。".format(time))
+        return []
+
+
+class MobilePolicy(KerasPolicy):
+    def model_architecture(self, num_features, num_actions, max_history_len):
+        """Build a Keras model and return a compiled model."""
+        from keras.layers import LSTM, Activation, Masking, Dense
+        from keras.models import Sequential
+
+        n_hidden = 32  # size of hidden layer in LSTM
+        # Build Model
+        batch_shape = (None, max_history_len, num_features)
+
+        model = Sequential()
+        model.add(Masking(-1, batch_input_shape=batch_shape))
+        model.add(LSTM(n_hidden, batch_input_shape=batch_shape))
+        model.add(Dense(input_dim=n_hidden, output_dim=num_actions))
+        model.add(Activation("softmax"))
+
+        model.compile(loss="categorical_crossentropy",
+                      optimizer="adam",
+                      metrics=["accuracy"])
+
+        logger.debug(model.summary())
+        return model
+
+
+def train_nlu():
+    from rasa_nlu.training_data import load_data
+    from rasa_nlu.config import RasaNLUModelConfig
+    from rasa_nlu.model import Trainer
+    from rasa_nlu import config
+
+    training_data = load_data("data/nlu.json")
+    trainer = Trainer(config.load("data/nlu_model_config.json"))
+    trainer.train(training_data)
+    model_directory = trainer.persist("models/", project_name="ivr", fixed_model_name="demo")
+
+    return model_directory
+
+
+def train_dialogue(domain_file="data/domain.yml",
+                   model_path="models/dialogue",
+                   training_data_file="data/stories.md"):
+    from rasa_core.featurizers import (MaxHistoryTrackerFeaturizer,
+                  BinarySingleStateFeaturizer)
+    featurizer = MaxHistoryTrackerFeaturizer(BinarySingleStateFeaturizer(), max_history=5)
+    agent = Agent(domain_file,
+                  policies=[MemoizationPolicy(max_history=5), KerasPolicy(featurizer)])
+
+    agent.train(
+        training_data_file,
+        epochs=200,
+        batch_size=16,
+        augmentation_factor=50,
+        validation_split=0.2
+    )
+
+    agent.persist(model_path)
+    return agent
+
+def run_ivrbot_online(input_channel=ConsoleInputChannel(),
+                      interpreter=RasaNLUInterpreter("models/ivr/demo"),
+                      domain_file="data/domain.yml",
+                      training_data_file="data/stories.md"):
+    from rasa_core.featurizers import (MaxHistoryTrackerFeaturizer,
+                  BinarySingleStateFeaturizer)
+    featurizer = MaxHistoryTrackerFeaturizer(BinarySingleStateFeaturizer(), max_history=5)
+    agent = Agent(domain_file,
+                  policies=[MemoizationPolicy(max_history=5), KerasPolicy(featurizer)],
+                  interpreter=interpreter)
+
+    agent.train_online(training_data_file,
+                       input_channel=input_channel,
+                       batch_size=50,
+                       epochs=200,
+                       max_training_samples=300)
+
+    return agent
+
+
+def run(serve_forever=True):
+    agent = Agent.load("models/dialogue",
+                       interpreter=RasaNLUInterpreter("models/ivr/demo"))
+
+    if serve_forever:
+        agent.handle_channel(ConsoleInputChannel())
+    return agent
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level="INFO")
+
+    parser = argparse.ArgumentParser(
+        description="starts the bot")
+
+    parser.add_argument(
+        "task",
+        choices=["train-nlu", "train-dialogue", "run", "online-train"],
+        help="what the bot should do - e.g. run or train?")
+    task = parser.parse_args().task
+
+    # decide what to do based on first parameter of the script
+    if task == "train-nlu":
+        train_nlu()
+    elif task == "train-dialogue":
+        train_dialogue()
+    elif task == "run":
+        run()
+    elif task == "online-train":
+        run_ivrbot_online()
+    else:
+        warnings.warn("Need to pass either 'train-nlu', 'train-dialogue', 'run' or 'online-train' to use the script.")
+        exit(1)
+
